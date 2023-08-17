63b88a14a0189dca923ff60a88672bfefa77f1cf
EnfrijoladaDeFrijol <arturomezsanchez@gmail.com>
2023-08-17 09:36:18 -0600

    [FEATURE] Se agregó la busqueda de la carpeta a organizar

--- organizador.sh
+++ organizador.sh
@@ -21,12 +21,29 @@ solicitarCarpeta(){
     read carpeta
 }
 
+buscarCarpeta(){
+    # Se quiere buscar la carpeta a organizar
 
+    # 1. Primero obtenemos la ubicación de la carpeta
+    ubiCarpeta=$(find ~/ -name "$carpeta")
+    echo $ubiCarpeta
+
+    # 2. Valisamos que esa ubicación exista en el SO
+    if [ -d "$ubiCarpeta" ] # La carpeta existe
+    then
+        bandera=1 # Decimos que si existe
+    else # La carpeta no existe en el So
+        bandera=0 # Para decir que no existe
+    fi
+}
 
 main(){
     imprimirTitulo
     imprimirLinea
     solicitarCarpeta
+    buscarCarpeta
+
+    printf "$bandera \n"
 }
 
 main
