60d52fc4ae77c61bbfc4611744518a08c97ffcce
EnfrijoladaDeFrijol <arturomezsanchez@gmail.com>
2023-08-16 23:55:02 -0600

    [FEATURE] Se crepo el archivo '.sh'

---
+++ organizador.sh
@@ -0,0 +1,14 @@
+
+imprimirTitulo(){
+    printf "\t   ___                     _            _         "
+    printf "\t  / _ \ _ _ __ _ __ _ _ _ (_)_____ _ __| |___ _ _ "
+    printf "\t | (_) | '_/ _` !  ` | ' \| |_ / _` !  ` / _ \ '_!"
+    printf "\t  \___/|_| \__, \__,_|_||_|_/__\__,_\__,_\___/_|  "
+    printf "\t           |___/                                  "
+}
+
+main(){
+    printf " O R G A N I Z A D O R "
+}
+
+main
