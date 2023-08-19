8a7a4b4403a948f7ac2327b0bde183c1554e6625
EnfrijoladaDeFrijol <arturomezsanchez@gmail.com>
2023-08-18 23:28:45 -0600

    [FEATURE] Se agregó comando de creación de carpetas donde se ordenarán los aechivos

--- organizador.sh
+++ organizador.sh
@@ -16,34 +16,58 @@ imprimirLinea(){
     printf "\n\t --------------------------------------------- \n"
 }
 
-solicitarCarpeta(){
+solicitarNombreCarpeta(){
     printf "\n\t Carpeta que desea organizar: "
     read carpeta
 }
 
+# función que trabaja con una bandera para saber si existe 
+# la carpeta y con esto poder seguir con elflujo del programa
 buscarCarpeta(){
     # Se quiere buscar la carpeta a organizar
 
     # 1. Primero obtenemos la ubicación de la carpeta
     ubiCarpeta=$(find ~/ -name "$carpeta")
-    echo $ubiCarpeta
+    #echo $ubiCarpeta
 
     # 2. Valisamos que esa ubicación exista en el SO
     if [ -d "$ubiCarpeta" ] # La carpeta existe
     then
-        bandera=1 # Decimos que si existe
+        bandera=1 # Bandera para sager si existe
     else # La carpeta no existe en el So
         bandera=0 # Para decir que no existe
+    fi
+}
+
+# Función que crea las carpetas en donde se irán
+# moviendo los documentos a organizar
+# Nota: aún falta hacer la versión en inglés del SO
+creacionDeCarpetas(){
+    mkdir ~/Documentos/Archivos_organizados
+}
+validarCarpetaOrganizada(){
+    printf "\t Verificando información...\n"
+    sleep 2
+    rutaCarpetaOrganizada=~/Documentos/Archivos_organizados
+    if [ -d "$rutaCarpetaOrganizada" ] # La carpeta existe
+    then # Carpeta organizada existe
+        printf "\n\t Ya existe la carpeta\n"
+    else # Carpeta organizada no existe
+        printf "\t Crando carpetas...\n"
+        sleep 1
+        #mkdir ~/Documentos/Archivos_organizados
+        printf "\t Se creó la carpeta organizada"        
     fi
 }
 
 main(){
     imprimirTitulo
     imprimirLinea
-    solicitarCarpeta
+    solicitarNombreCarpeta
     buscarCarpeta
 
-    printf "$bandera \n"
+    validarCarpetaOrganizada
+    #printf "$bandera \n"
 }
 
 main
